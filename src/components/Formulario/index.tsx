97d078088ddd84f8753d28b7266da5634ab40a5d
Sponchiatto <rafael_sponchiatto@hotmail.com>
2024-07-08 17:55:58 -0300

    Little project - studies

---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,34 @@
+import Botao from "../Botao";
+import style from "./Formulario.module.scss";
+const Formulario = () => {
+  return (
+    <form className={style.novaTarefa}>
+      <div className={style.inputContainer}>
+        <label htmlFor="tarefa">Adiciona um novo estudo</label>
+        <input
+          type="text"
+          name="tarefa"
+          id="tarefa"
+          placeholder="O que você vai estudar?"
+          required
+        />
+      </div>
+
+      <div className={style.inputContainer}>
+        <label htmlFor="tempo">Tempo</label>
+        <input
+          type="time"
+          step="1"
+          name="tempo"
+          id="tempo"
+          min="00:00:00"
+          max="1:30:00"
+          required
+        />
+      </div>
+      <Botao />
+    </form>
+  );
+};
+
+export default Formulario;
